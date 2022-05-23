354039689d80eddc5e41082d5772212079a58a2c
syukurm <49692442+syukurm@users.noreply.github.com>
2022-05-24 03:19:41 +0700

    refactor: :label: rename parameter name to command

--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -2,7 +2,7 @@ import type { CommandInteraction, Message } from "discord.js";
 import type { SimpleCommandMessage } from "discordx";
 
 export interface Command {
-  execute(interaction: CommandInteraction | Message): Promise<void>;
+  execute(command: CommandInteraction | Message): Promise<void>;
   simpleCommand(command: SimpleCommandMessage): Promise<void>;
   slash(interaction: CommandInteraction): Promise<void>;
 }
