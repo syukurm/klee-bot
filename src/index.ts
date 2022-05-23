0d841d1618805582ec9d7fb042945a127bc4809d
syukurm <49692442+syukurm@users.noreply.github.com>
2022-05-24 00:30:45 +0700

    refactor(bot): :fire: remove logger

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import { dirname, importx } from "@discordx/importer";
 import { DIService } from "discordx";
 import { Container } from "typedi";
 
+import ConfigService from "~services/config.service";
+
 import Bot from "./bot";
-import ConfigService from "./services/config.service";
-import LoggerService from "./services/logger.service";
 
+// Setup IOC using typedi
 DIService.container = Container;
-const config = Container.get(ConfigService);
-const logger = Container.get(LoggerService);
 
-const bot = new Bot(logger);
+const config = Container.get(ConfigService);
+
+const bot = new Bot();
+
+// Load all events and commands
 await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
 await bot.login(config.get("BOT_TOKEN"));
