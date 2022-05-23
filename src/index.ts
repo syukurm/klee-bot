ef654a9e27b17a6b4dffafcb395a074e117528dd
syukurm <49692442+syukurm@users.noreply.github.com>
2022-05-24 03:15:44 +0700

    refactor(bot): :loud_sound: add some logging

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { DIService } from "discordx";
 import { Container } from "typedi";
 
 import ConfigService from "~services/config.service";
+import LoggerService from "~services/logger.service";
 
 import Bot from "./bot";
 
@@ -12,10 +13,14 @@ import Bot from "./bot";
 DIService.container = Container;
 
 const config = Container.get(ConfigService);
+const logger = Container.get(LoggerService).logger.withScope("bot").withTag("start");
 
+logger.start("Starting the bot");
 const bot = new Bot();
 
 // Load all events and commands
 await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
-await bot.login(config.get("BOT_TOKEN"));
+await bot.login(config.botToken);
+
+logger.ready("Bot started");
