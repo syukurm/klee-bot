49bcab3759be21afc12e530a37f43e2ef6d1f0de
syukurm <49692442+syukurm@users.noreply.github.com>
2022-05-29 01:16:48 +0700

    feat(command): :art: add new features and improve code structure
    add hint, new game, give up, and resend buttons, add start game to context menu, multiple instances

--- src/commands/hangman.command.ts
+++ src/commands/hangman.command.ts
@@ -1,36 +1,23 @@
-import { CommandInteraction, Message, MessageCollector, MessageEmbed } from "discord.js";
-import { Discord, SimpleCommand, SimpleCommandMessage, Slash } from "discordx";
-import ms from "ms";
+import type { Consola } from "consola";
+import { ButtonInteraction, CommandInteraction, Message, UserContextMenuInteraction } from "discord.js";
+import { ButtonComponent, ContextMenu, Discord, SimpleCommand, SimpleCommandMessage, Slash } from "discordx";
 
-import emojis from "~constants/letter-emojis.constant";
-import wordlist from "~constants/wordlist.constant";
+import HangmanGame from "~classes/HangmanGame";
+import { CUSTOM_IDS } from "~constants/hangman.constant";
 import type { Command } from "~interfaces/command";
+import LoggerService from "~services/logger.service";
+
+const SOMETHING_WENT_WRONG = "Something went wrong.";
 
 @Discord()
-export default class Hangman implements Command {
-  private readonly maxMisses = 7;
+export default class HangmanCommand implements Command {
+  private games: Map<string, HangmanGame>;
 
-  private isPlaying: boolean;
+  private logger: Consola;
 
-  private timeGameStarted: Date;
-
-  private currentMistake: number;
-
-  private currentWord = "";
-
-  private wrongCharacters: Set<string>;
-
-  private correctCharacters: Set<string>;
-
-  private gameText: Message | undefined;
-
-  public constructor() {
-    this.isPlaying = false;
-    this.timeGameStarted = new Date();
-    this.currentMistake = 0;
-    this.currentWord = "";
-    this.wrongCharacters = new Set();
-    this.correctCharacters = new Set();
+  public constructor(loggerService: LoggerService) {
+    this.logger = loggerService.logger.withScope("command").withTag("hangman");
+    this.games = new Map();
   }
 
   @SimpleCommand("hangman", { description: "Play a game of hangman" })
@@ -38,137 +25,74 @@ export default class Hangman implements Command {
     await this.execute(command.message);
   }
 
+  @ContextMenu("USER", "Hangman Game")
   @Slash("hangman", { description: "Play a game of hangman" })
-  public async slash(interaction: CommandInteraction): Promise<void> {
+  public async slash(interaction: CommandInteraction | UserContextMenuInteraction): Promise<void> {
     await this.execute(interaction);
   }
 
-  public async execute(interaction: CommandInteraction | Message): Promise<void> {
-    if (this.isPlaying) {
-      await interaction.reply("Game is already in progress.");
+  public async execute(command: CommandInteraction | UserContextMenuInteraction | Message): Promise<void> {
+    let game: HangmanGame;
+    if (this.games.has(command.channelId)) {
+      game = this.games.get(command.channelId) as HangmanGame;
+      if (game.isPlaying) {
+        await command.reply("Game is already in progress.");
+        return;
+      }
     } else {
-      await interaction.reply(`Starting a new game`);
-      await this.start(interaction);
+      game = new HangmanGame();
+      this.games.set(command.channelId, game);
+    }
+    await game.start(command);
+  }
+
+  private getGame(channelId: string): HangmanGame | undefined {
+    return this.games.get(channelId);
+  }
+
+  @ButtonComponent(CUSTOM_IDS.HINT_BUTTON)
+  public async handleHintButton(interaction: ButtonInteraction): Promise<void> {
+    try {
+      const game = this.getGame(interaction.channelId);
+      await game?.giveAHint();
+      await interaction.reply(`<@${interaction.user.id}> used a hint`);
+    } catch (error) {
+      this.logger.error(error);
+      await interaction.reply({ ephemeral: true, content: SOMETHING_WENT_WRONG });
     }
   }
 
-  public async start(interaction: CommandInteraction | Message): Promise<void> {
-    this.isPlaying = true;
-    this.timeGameStarted = new Date();
-
-    const randomWord = wordlist[Math.floor(Math.random() * wordlist.length)] ?? "Nothing";
-    this.currentWord = randomWord.toLowerCase();
-
-    const gameInterface = this.createGameInterface();
-    this.gameText = await interaction.channel?.send({ embeds: [gameInterface] });
-
-    if (this.gameText === undefined) {
-      this.isPlaying = false;
-      return;
-    }
-
-    const collector = interaction.channel?.createMessageCollector({
-      time: ms("1"),
-      filter: (message) => /^[A-Za-z]+$/.test(message.content),
-    });
-
-    collector?.on("collect", async (message) => {
-      const messageContent = message.content.trim().toLowerCase();
-
-      if (message.content.length === 1) {
-        if (this.currentWord.includes(messageContent)) {
-          if (!this.correctCharacters.has(messageContent)) {
-            await this.gameText?.delete();
-            this.gameText = await interaction.channel?.send({ embeds: [this.createGameInterface()] });
-          }
-          this.correctCharacters.add(messageContent);
-        } else if (!this.wrongCharacters.has(messageContent)) {
-          this.currentMistake += 1;
-          this.wrongCharacters.add(messageContent);
-        }
-      } else if (messageContent === this.currentWord) {
-        [...messageContent].forEach((letter) => this.correctCharacters.add(letter));
-      }
-      await this.updateGame(collector);
-    });
-
-    collector?.on("end", async () => {
-      if (this.isPlaying) {
-        this.isPlaying = false;
-        await this.updateGame(collector);
-      }
-    });
-  }
-
-  public createGameInterface(): MessageEmbed {
-    const images = [
-      "https://i.imgur.com/on7dmN6.png",
-      "https://i.imgur.com/nbZ8Akb.png",
-      "https://i.imgur.com/lSjCWM8.png",
-      "https://i.imgur.com/ylmGiN5.png",
-      "https://i.imgur.com/D4MIRYD.png",
-      "https://i.imgur.com/PDMNT8U.png",
-      "https://i.imgur.com/egjJoc2.png",
-      "https://i.imgur.com/hQ9gCCf.png",
-    ];
-
-    const guessedWord = [...this.currentWord].map((letter) => {
-      if (this.correctCharacters.has(letter)) {
-        return emojis[letter.toUpperCase()] as string;
-      }
-      return "⬜";
-    });
-
-    const guessesLeft = this.maxMisses - this.currentMistake;
-
-    const gameInterface = new MessageEmbed()
-      .setColor("#60a5fa")
-      .setTitle("Hangman Game")
-      .addField("Guess the word", guessedWord.join(" "))
-      .setImage(images[guessesLeft] ?? "https://i.imgur.com/on7dmN6.png")
-      .setTimestamp(this.timeGameStarted)
-      .setFooter({ text: `${this.maxMisses - this.currentMistake} guesses left` });
-
-    if (this.wrongCharacters.size > 0) {
-      const wrongCharacters = [...this.wrongCharacters].map((letter) => emojis[letter.toUpperCase()] as string);
-      gameInterface.addField("Wrong characters:", wrongCharacters.join(" "));
-    }
-
-    if (!this.isPlaying) {
-      const noMoreGuessesLeft = this.currentMistake >= this.maxMisses;
-      const hasWordBeenGuessed = [...this.currentWord].every((letter) => this.correctCharacters.has(letter));
-
-      if (noMoreGuessesLeft) {
-        gameInterface.setColor("#ef4444").setAuthor({ name: `Game has ended, the word was ${this.currentWord}` });
-      } else if (hasWordBeenGuessed) {
-        gameInterface.setColor("#22c55e").setAuthor({ name: `Game has ended, you guessed the word` });
-      } else {
-        gameInterface.setColor("#475569").setAuthor({ name: `Game has ended` });
-      }
-    }
-
-    return gameInterface;
-  }
-
-  public async updateGame(collector: MessageCollector): Promise<void> {
-    const noMoreGuessesLeft = this.currentMistake >= this.maxMisses;
-    const hasWordBeenGuessed = [...this.currentWord].every((letter) => this.correctCharacters.has(letter));
-
-    if (noMoreGuessesLeft || hasWordBeenGuessed) {
-      this.isPlaying = false;
-      const updatedInterface = this.createGameInterface();
-      await this.gameText?.edit({ embeds: [updatedInterface] });
-      this.reset();
-      collector.stop();
-    } else {
-      const updatedInterface = this.createGameInterface();
-      await this.gameText?.edit({ embeds: [updatedInterface] });
+  @ButtonComponent(CUSTOM_IDS.GIVE_UP_BUTTON)
+  public async handleGiveUpButton(interaction: ButtonInteraction): Promise<void> {
+    try {
+      const game = this.getGame(interaction.channelId);
+      await game?.giveUp();
+      await interaction.reply(`<@${interaction.user.id}> gave up`);
+    } catch (error) {
+      this.logger.error(error);
+      await interaction.reply({ ephemeral: true, content: SOMETHING_WENT_WRONG });
     }
   }
 
-  public reset(): void {
-    this.correctCharacters = new Set<string>();
-    this.wrongCharacters = new Set<string>();
-    this.currentMistake = 0;
+  @ButtonComponent(CUSTOM_IDS.NEW_WORD_BUTTON)
+  public async handleNewWordButton(interaction: ButtonInteraction): Promise<void> {
+    try {
+      const game = this.getGame(interaction.channelId);
+      await game?.restart(interaction);
+    } catch (error) {
+      this.logger.error(error);
+      await interaction.reply({ ephemeral: true, content: SOMETHING_WENT_WRONG });
+    }
+  }
+
+  @ButtonComponent(CUSTOM_IDS.RESEND_BUTTON)
+  public async handleResendButton(interaction: ButtonInteraction): Promise<void> {
+    try {
+      const game = this.getGame(interaction.channelId);
+      await game?.resendInterface();
+    } catch (error) {
+      this.logger.error(error);
+      await interaction.reply({ ephemeral: true, content: SOMETHING_WENT_WRONG });
+    }
   }
 }
---
+++ src/constants/hangman.constant.ts
@@ -0,0 +1,19 @@
+export const hangmanImagesUrl = [
+  "https://i.imgur.com/T6kWN6x.png",
+  "https://i.imgur.com/dYf0zk3.png",
+  "https://i.imgur.com/kQ1LfSF.png",
+  "https://i.imgur.com/RbDdBWJ.png",
+  "https://i.imgur.com/ZnI9Yjk.png",
+  "https://i.imgur.com/SlfQp1w.png",
+  "https://i.imgur.com/P2IP35g.png",
+  "https://i.imgur.com/2HRI1ru.png",
+  "https://i.imgur.com/LWhm7l4.png",
+  "https://i.imgur.com/alSr9gk.png",
+] as const;
+
+export const CUSTOM_IDS = {
+  HINT_BUTTON: "hangman-hint-button",
+  GIVE_UP_BUTTON: "hangman-give-up-button",
+  NEW_WORD_BUTTON: "hangman-new-word-button",
+  RESEND_BUTTON: "hangman-resend-button",
+};
